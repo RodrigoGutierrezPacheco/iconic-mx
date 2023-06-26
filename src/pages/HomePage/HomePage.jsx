f85d63a07fb4a65190578666fac71e5a5ccb6a23
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-06-26 17:50:13 -0600

    modificacion pagina principal

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,16 +3,16 @@ import "./HomePage.css";
 function HomePage() {
   return (
     <div className="home">
-			<div>
-			<img src="images/carrito.png" alt="" className="png1" />
-			<div className="row-portada">
+			<div className="bg-color-portada">
+			<img src="images/carrito.png" alt="" className="png1 bg-color-portada" />
+			<div className="row-portada ">
 				<img src="images/etiqueta.png" alt="" className="png" />
 				<img src="images/boton.png" alt="" className="png" />
 				<img src="images/playera.png" alt="" className="png" />
 				<img src="images/sudadera.png" alt="" className="png" />
 			</div>
-			</div>
 			<img className="iconic-portada" src="images/iconic.png" alt="" />
+			</div>
 			<div className="column-portada">
 				<span className="inicio-portada subtitle">Inicio</span>
 				<span className="tienda-portada subtitle">Tienda</span>
@@ -30,37 +30,49 @@ function HomePage() {
 			<div className="descubre">
 				<div className="row-descubre">
 					<h1 className="title-descubre">Descubre</h1>
-					<img src="images/lupa.png" alt="" className="png2 end" />
 				</div>
 				<h2 className="justify">Moda con historia. <br />Unete a nosotros y lleva un pedacido de significado en cada prenda.</h2>
+				<div className="flex-redes">
+					<img className="png4" src="images/fb.png" alt="" />
+					<img className="png4" src="images/tw.png" alt="" />
+					<img className="png4" src="images/whats.png" alt="" />
+					<img className="png4" src="images/insta.png" alt="" />
+				</div>
 				<img src="images/ali.webp" alt="" className="img marginb" />
 			</div>
-			<div className="productos">
+			<div>
 				<div className="row-descubre">
-					<h1 className="title-descubre start justify">Nuestra inspiracion</h1>
-					<img src="images/inspiracion.png" alt="" className="png2" />
+					<h1 className="title-descubre marginl start justify">Nuestra inspiracion</h1>
 				</div>
 				<h2 className="justify marginl">Nos inspiramos en momentos cruciales que marcaron a la humanidad. Descubre neustra coleccion que fusiona estilo y significado. Unete a nosotros y lleva contigo un pedacito de historia en cada prenda.</h2>
-				<img src="images/checo.jpg" alt="" className="img marginb" />
+				<img src="images/checo.jpg" alt="" className="img-checo marginb" />
 			</div>
 			<div className="descubre">
 				<div className="row-descubre">
 					<h1 className="title-descubre start">Nosotros</h1>
-					<img src="images/nosotros.png" alt="" className="png2" />
 				</div>
-				<h2 className="justify maginl">En icon, hacemos moda que inspira. Descubre nuestra seleccoin de ropa de calidad y escilo excepcionales. Cada prenda esta disenada para expresar tu personalidad y confianza. Unete a nosotros y encuentra tu simbolo de estilo y empoderamiento.</h2>
+				<h2 className="justify maginl en-icon">En icon, hacemos moda que inspira. Descubre nuestra seleccoin de ropa de calidad y escilo excepcionales. Cada prenda esta disenada para expresar tu personalidad y confianza. Unete a nosotros y encuentra tu simbolo de estilo y empoderamiento.</h2>
 				<div className="column">
-				<div className="container">
+				<div className="container margint">
+					<img src="images/pildora.png" alt="Imagen de fondo" className="png30" />
+					<div className="text column">
+						<span className="strong">10</span>
+						<span>Modelos diferentes</span>
+					</div>
+				</div>
+				<div className="container margint">
 					<img src="images/circulo1.png" alt="Imagen de fondo" className="png3" />
-					<h2 className="text">10 <br /> Modelos diferentes.</h2>
+					<div className="text column margint">
+						<span className="strong">10</span>
+						<span>Moentos Historicos</span>
+					</div>
 				</div>
-				<div className="container">
-					<img src="images/circulo2.png" alt="Imagen de fondo" className="png3" />
-					<h2 className="text">Moda Sustentable</h2>
-				</div>
-				<div className="container">
-					<img src="images/circulo3.png" alt="Imagen de fondo" className="png3" />
-					<h2 className="text">Moda para Todos</h2>
+				<div className="container margint">
+					<img src="images/circulo.png" alt="Imagen de fondo" className="png3" />
+					<div className="text column margint">
+						<span className="strong">100+</span>
+						<span>Clientes</span>
+					</div>
 				</div>
 				</div>
 			</div>
